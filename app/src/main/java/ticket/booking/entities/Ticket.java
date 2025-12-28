198f74e814773802ecbe13e920fc02a5d0f45058
avinash.yaddlapalli <avinash.yaddlapalli@super.money>
2025-12-28 15:51:16 +0530

    added user login signup and utils

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -1,6 +1,7 @@
 package ticket.booking.entities;
 
 import java.util.Date;
+import java.util.List;
 
 public class Ticket {
 
@@ -11,4 +12,62 @@ public class Ticket {
     private Date dateOfTravel;
 
     private Train train;
+
+    public Ticket(String ticketId, String source, String destination, String userId, Date dateOfTravel, Train train){
+        this.ticketId = ticketId;
+        this.source = source;
+        this.destination = destination;
+        this.userId = userId;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket ID: %s belongs to user: %s from: %s to: %s on: %s", ticketId, userId, source, destination, dateOfTravel);
+    }
+
+    public Ticket(){}
+
+    public String getTicketId(){
+        return ticketId;
+    }
+    public String getSource(){
+        return source;
+    }
+    public String getDestination(){
+        return destination;
+    }
+    public String getUserId(){
+        return userId;
+    }
+    public Date getDateOfTravel(){
+        return dateOfTravel;
+    }
+    public Train getTrain(){
+        return train;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public void setDateOfTravel(Date dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
 }
--- app/src/main/java/ticket/booking/entities/Train.java
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -11,7 +11,53 @@ public class Train {
 
     private List<List<Integer>> seats;
 
-    private Map<String, Time> stationTimes;
+    private Map<String, String> stationTimes;
 
     private List<String> stations;
+
+    public Train(){}
+
+    public Train(String trainId, String trainNo, List<List<Integer>> seats, List<String> stations, Map<String, String> stationTimes){
+        this.trainId = trainId;
+        this.trainNo = trainNo;
+        this.seats = seats;
+        this.stations = stations;
+        this.stationTimes = stationTimes;
+    }
+
+    public String getTrainId(){
+        return trainId;
+    }
+    public String getTrainNo(){
+        return trainNo;
+    }
+    public List<List<Integer>> getSeats(){
+        return seats;
+    }
+    public List<String> getStations(){
+        return stations;
+    }
+    public Map<String, String> getStationTimes(){
+        return stationTimes;
+    }
+
+    public void setTrainId(String trainId){
+        this.trainId = trainId;
+    }
+    public String settrainNo(){
+        return trainNo;
+    }
+    public List<String> setstations() {
+        return stations;
+    }
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+    public void setStationTimes(Map<String, String> stationTimes) {
+        this.stationTimes = stationTimes;
+    }
+
+    public String getTrainInfo(){
+        return String.format("Train ID: %s Train NO: %s", trainId, trainNo);
+    }
 }
